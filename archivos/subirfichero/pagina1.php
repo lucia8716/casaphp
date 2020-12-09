5e30b6f38ef7ac85f080001059b0469e1bb68d8c
Lucia8716 <crespopopo@gmail.com>
2020-12-10 00:11:05 +0100

    v1

---
+++ archivos/subirfichero/pagina1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <title>Insert title here</title>
+</head>
+
+<body>
+    <H2>Seleccionar un archivo para enviar al servidor</H2>
+    <form action="pagina2.php" method="post" enctype="multipart/form-data">
+        Seleccione el archivo:
+        <input type="file" name="foto"><br />
+        <input type="submit" value="Enviar">
+        <INPUT type='hidden' name='max_file_size' value='2500000'>
+    </form>
+
+
+</body>
+
+</html>
